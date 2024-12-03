b6019af0c2c118f58cf230281792c61a71f5809c
bucketteOfIvy <ashewimer@gmail.com>
2024-12-02 22:16:31 -0600

    reran with new pca approach

--- itr_3/scripts/2_pcas.py
+++ itr_3/scripts/2_pcas.py
@@ -10,7 +10,7 @@ from pyspark.ml.linalg import Vectors
 from pyspark.ml.feature import StandardScaler, PCA
 import pyspark.sql.functions as F
 
-def run_pca(pca_features, min_components, num_features, result_prefix=''):
+def run_pca(features, feature_names, min_components, result_prefix=''):
     # Repeat PCA with one new component every time until 
     # we capture enough variance.
     num_components = min_components
@@ -19,23 +19,23 @@ def run_pca(pca_features, min_components, num_features, result_prefix=''):
     while not explains_enough_variance: 
         # Set up PCA
         pca = PCA(k=num_components, inputCol="features", outputCol='pcaFeatures')
-        pca_model = pca.fit(pca_features)
+        pca_model = pca.fit(features)
 
         # Have we explained enough variance?
         expl_vars = pca_model.explainedVariance.toArray()
         total_explained_variance = sum(pca_model.explainedVariance.toArray())
         print(f"Explained {total_explained_variance} with {num_components} components.")
-        if total_explained_variance >= 0.8 or expl_vars[-1] < 1/num_features:
+        if total_explained_variance >= 0.8 or float(expl_vars[-1]) < 1/len(feature_names):
             print("Saving principal components.")
             # Save component info
             expl_vars = pca_model.explainedVariance.toArray()
             pca_loadings = pca_model.pc.toArray()
             rows = [(i, expl_vars[i], *pca_loadings[:, i]) for i in range(num_components)]
-            loading_df = pd.DataFrame(rows, columns=['component', 'explained_var'] + pca_feature_cols)
+            loading_df = pd.DataFrame(rows, columns=['component', 'explained_var'] + feature_names)
             loading_df.to_csv(f'../data/{result_prefix}_pca_loadings.csv', header=True)
 
             # Save transformed data
-            pca_results = pca_model.transform(pca_features).select('pcaFeatures')
+            pca_results = pca_model.transform(features).select('pcaFeatures')
             pca_results.write.mode('overwrite').parquet(f'../data/{result_prefix}_pca_results.parquet')
 
             # End loop
@@ -55,7 +55,7 @@ spark = SparkSession \
 # negative crash outcomes. Hence this approach!
 pca_feature_cols = ["perBlack", "unempl", "percentNoHs", 'perWithoutIns', "medHouseIncome"]
 financial_pca_cols = ["medHouseIncome", "unemplRate", 'medHouseVal', 'perOwnerOcc', 'workTravelAv']
-med_fric_pca_cols = ['percentNoHs', 'percentBach', 'perBlack', 'perWithoutIns']
+med_fric_pca_cols = ['percentNoHs', 'percentBach', 'perBlack', 'perWithoutIns', 'perOver65']
 all_cols = list(set(pca_feature_cols + financial_pca_cols + med_fric_pca_cols))
 
 #### Preprocessing
@@ -94,6 +94,7 @@ pca_features.persist()
 fin_pca_features.persist()
 medfric_pca_features.persist()
 
-run_pca(pca_features, 2, "", 5)
-run_pca(fin_pca_features, 2, "finance", 5)
-run_pca(medfric_pca_features, 2, "medical_friction", 4)
+run_pca(pca_features, pca_feature_cols, 2, "")
+run_pca(fin_pca_features, financial_pca_cols, 2, "finance")
+run_pca(medfric_pca_features, med_fric_pca_cols, 2, "medical_friction")
+
