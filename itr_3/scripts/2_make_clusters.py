0da4974e5d203654f7589deedb0487cc5737c9c8
Ashlynn Ivy Wimer <ashewimer@gmail.com>
2024-12-02 19:54:42 -0600

    okay pca should really work now

--- itr_3/scripts/2_make_clusters.py
+++ itr_3/scripts/2_make_clusters.py
@@ -86,7 +86,7 @@ while not explains_enough_variance:
         pca_loadings = pca_model.pc.toArray()
         rows = [(i, expl_vars[i], *pca_loadings[:, i]) for i in range(num_components)]
         print(rows)
-        loading_df = pd.DataFrame(rows, index=['component', 'explained_var'] + pca_feature_cols)
+        loading_df = pd.DataFrame(rows, header=['component', 'explained_var'] + pca_feature_cols)
         loading_df.to_csv('../data/pca_loadings.csv', header=True)
 
         # Save transformed data
